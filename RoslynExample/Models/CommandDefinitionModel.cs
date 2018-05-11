85a4e1154cb330b4a18767266426a7dee7947f51
Chris Berthold <cberthold@baxtertechnology.com>
2018-05-11 16:47:46 -0400

    add command definition model for building the command class

---
+++ RoslynExample/Models/CommandDefinitionModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RoslynExample.Metadata;
+
+namespace RoslynExample.Models
+{
+    public class CommandDefinitionModel
+    {
+        public string Namespace { get; set; } = "Command";
+        public string ClassName { get; set; }
+        public EntityMetadata InputMetadata { get; set; }
+    }
+}
