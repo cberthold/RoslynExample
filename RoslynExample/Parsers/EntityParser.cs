2c131e6aed027455f8daba79b5e4664487558bcf
Chris Berthold <cberthold@baxtertechnology.com>
2018-05-11 16:47:09 -0400

    setup basic structure for the entity metadata parser

--- RoslynExample/Parsers/EntityParser.cs
+++ RoslynExample/Parsers/EntityParser.cs
@@ -3,10 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using RoslynExample.Metadata;
 
 namespace RoslynExample.Parsers
 {
-    class EntityParser
+    public class EntityParser
     {
+        public EntityMetadata Parse(ClassDeclarationSyntax syntax)
+        {
+            var entity = new EntityMetadata();
+
+            // TODO: Set class name of input dto
+            entity.ClassName = "";
+
+            // TODO: Set the namespace of the class
+            entity.ClassNamespace = "";
+
+            return entity;
+        }
     }
 }
